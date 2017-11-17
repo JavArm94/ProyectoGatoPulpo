96b19ce01e44adbfaa157fe9f83914cee186c903
JavArm94 <33666838+JavArm94@users.noreply.github.com>
2017-11-17 11:49:09 -0300

    Add files via upload

--- ControladorNuevo.java
+++ ControladorNuevo.java
@@ -1,19 +1,16 @@
 package Controlador;
 
-import java.awt.Image;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import ConexiónBase.Database;
 import Modelo.Butaca;
@@ -23,9 +20,10 @@ import Modelo.Funcion;
 import Modelo.Pelicula;
 import Modelo.Socio;
 import Vista.VistaNueva;
+import org.apache.commons.lang3.StringUtils;
 
-public class ControladorNuevo  implements MouseListener,ActionListener{
-
+public class ControladorNuevo  implements MouseListener,ActionListener,FocusListener{
+    
 	private Butaca butaca;
 	private Cine cine;
 	private Cliente cliente;
@@ -34,12 +32,14 @@ public class ControladorNuevo  implements MouseListener,ActionListener{
 	private Socio socio;
 	private  ArrayList<Butaca> Sala = new ArrayList <Butaca>();
 	private VistaNueva vn;
-	//private Database db = Database.getInstance();
+	private Database db = Database.getInstance();
 	private ControladorLogin cl;
+
 	
 	public ControladorNuevo () {
 		this.setVn(new VistaNueva(this));
 		vn.setVisible(true);
+		//if (db.esAdmin(vn.get))
 	        
 		// ACA SE DEBERIA CONTROLAR SI NO TIENE PRIVILEGIOS INICIA ASI
 	        /*	System.out.println("tiene privilegios");
@@ -62,10 +62,10 @@ public class ControladorNuevo  implements MouseListener,ActionListener{
 		
 		this.setButaca(new Butaca());
 		this.setCine(new Cine());		
-		this.setCliente(new Cliente(null, null, null, null));
+		this.setCliente(new Cliente(null, null, null));
 		this.setFuncion(new Funcion(null,null,Sala));
 		this.setPelicula(new Pelicula());
-		this.setSocio(new Socio(null, null, null, null, null, null));
+		this.setSocio(new Socio(null, null, null, null, null));
 		
 		/*	
 		cine.getFunciones().add(funcion);
@@ -76,7 +76,25 @@ for (int i = 0; i < 54; i++) {
 		}*/
 	}
 
-
+	public ControladorNuevo(Boolean es_admin){
+		this.setVn(new VistaNueva(this));
+		vn.setVisible(true);
+		vn.getInforme_Usu().setVisible(true);
+		vn.getVenta_Usu().setVisible(true);
+		vn.getVentas().setVisible(true);
+		vn.getSocio().setVisible(false);
+		vn.getPelicula_Cartelera().setVisible(false);
+		vn.getPelicula_Butacas().setVisible(false);
+		vn.getEntrada().setVisible(false);
+		vn.getUsuario_Registro().setVisible(false);
+		vn.getUsuario_Busqueda().setVisible(false);
+		vn.getInforme().setVisible(false);
+		vn.getUsuario_B().setVisible(false);
+		vn.getVenta_B().setVisible(false);
+		vn.getInformes_B().setVisible(false);
+		vn.getPrecio_Entrada_B().setVisible(false);
+		vn.getSocio_B().setVisible(false);
+	}
 	
 
 	public Butaca getButaca() {
@@ -199,12 +217,14 @@ for (int i = 0; i < 54; i++) {
 		
 	}
 
-
+    
 	@Override
 	public void actionPerformed(ActionEvent e) {	
 	// CADA COMTEMPLACION HACE QUE SE SETEEN EN FALSO
 		//LAS OTRAS VISTAS, HAY SETEOS INNECESARIOS
 		// ASI QUE HAY QUE ARMAR UN SINGLETON O ALGO
+		
+		//INICIO ---- BLOQUE DE CAMBIO DE VISTAS POR SOLAPAS O BOTONES
 		if(e.getSource()==vn.getVenta_B()){
 			vn.getVentas().setVisible(true);
 			vn.getSocio().setVisible(false);
@@ -225,7 +245,7 @@ for (int i = 0; i < 54; i++) {
 				vn.getUsuario_Registro().setVisible(false);
 				vn.getInforme().setVisible(true);
 			
-			}else if (e.getSource()==vn.getPrecio_Entrada_B() && vn.getPrecio_Entrada_B().getText().equals("Precio Entrada")){
+			}else if (e.getSource()==vn.getPrecio_Entrada_B()){
 				vn.getVentas().setVisible(false);
 				vn.getSocio().setVisible(false);
 				vn.getPelicula_Cartelera().setVisible(false);
@@ -234,7 +254,6 @@ for (int i = 0; i < 54; i++) {
 				vn.getUsuario_Registro().setVisible(false);
 				vn.getInforme().setVisible(false);
 				vn.getPelicula_Butacas().setVisible(false);
-				vn.getPrecio_Entrada_B().setText("Pelicula");
 				
 			}else if (e.getSource()==vn.getUsuario_B()){
 				vn.getVentas().setVisible(false);
@@ -245,7 +264,12 @@ for (int i = 0; i < 54; i++) {
 				vn.getUsuario_Registro().setVisible(false);
 				vn.getUsuario_Busqueda().setVisible(true);
 				vn.getInforme().setVisible(false);
-			
+			}else if(e.getSource()==vn.getAgregar_usuario_B()) {
+					vn.getUsuario_Busqueda().setVisible(false);
+					vn.getUsuario_Registro().setVisible(true);			
+			}else if (e.getSource()==vn.getAtras_Regi()){
+				vn.getUsuario_Busqueda().setVisible(true);
+				vn.getUsuario_Registro().setVisible(false);			
 			}else if (e.getSource()==vn.getSocio_B()){
 				vn.getVentas().setVisible(false);
 				vn.getSocio().setVisible(true);
@@ -254,10 +278,8 @@ for (int i = 0; i < 54; i++) {
 				vn.getEntrada().setVisible(false);
 				vn.getUsuario_Busqueda().setVisible(false);
 				vn.getUsuario_Registro().setVisible(false);
-				vn.getInforme().setVisible(false);
-				
-			
-			}else if (e.getSource()==vn.getPrecio_Entrada_B() && vn.getPrecio_Entrada_B().getText().equals("Pelicula")){
+				vn.getInforme().setVisible(false);			
+			}else if (e.getSource()==vn.getPelicula_B()){
 				vn.getVentas().setVisible(false);
 				vn.getSocio().setVisible(false);
 				vn.getPelicula_Cartelera().setVisible(true);
@@ -265,23 +287,27 @@ for (int i = 0; i < 54; i++) {
 				vn.getEntrada().setVisible(false);
 				vn.getUsuario_Busqueda().setVisible(false);
 				vn.getUsuario_Registro().setVisible(false);
-				vn.getInforme().setVisible(false);
-				vn.getPrecio_Entrada_B().setText("Precio Entrada");
-			
+				vn.getInforme().setVisible(false);			
 			} else if (e.getSource()==vn.getCerrar_Sesion()){
 				vn.setVisible(false);
 			
 			} else if (e.getSource()==vn.getInforme_Usu()){
 				vn.getInforme().setVisible(true);
 				vn.getVentas().setVisible(false);
-				System.out.println("h");
 			}else if (e.getSource()==vn.getVenta_Usu()){
 				vn.getVentas().setVisible(true);;
 				vn.getInforme().setVisible(false);
-				System.out.println("h");
 			}
- 		
+		//FIN ---- BLOQUE DE CAMBIO DE VISTAS POR SOLAPAS O BOTONES
+		
+		
+		
+		
+		
+		
 		}
+	
+	
 	/*	if(e.getSource().equals(vn.getBtnPrecioEntrada())){
 			// aca hay que crear un patron, ese que enciende y apaga
 		vn.getButacas_Usuario().setVisible(false);
@@ -324,10 +350,104 @@ for (int i = 0; i < 54; i++) {
 		this.cl = cl;
 	}
 
+	@Override
+	public void focusGained(FocusEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+
+	public void checkIngresoNumerico(FocusEvent a,JTextField b, JLabel c){
+		if (a.getSource() == b) {
+			if (b.getText().isEmpty()) {
+				c.setText("*No puede estar vacio");
+				
+			} else if (!StringUtils.isNumeric(b.getText())){
+				c.setText("*No puede contener letras");
+				
+			} else {c.setText(null);}}		
+	}
+
+	public void checkIngresoDni(FocusEvent a, JTextField b, JLabel c) {
+		if (a.getSource() == b) {
+			if (b.getText().isEmpty()) {
+				c.setText("*No puede estar vacio");
+			
+			} else if (!StringUtils.isNumeric(b.getText())) {
+				c.setText("*No puede contener letras");
+				
+			} else if (b.getText().length() != 8) {
+				c.setText("*Debe contener 8 digitos");
+				
+			} else {
+				c.setText(null);
+			}
+		}
+	}
+
+	public void checkIngresoLetras(FocusEvent a,JTextField b, JLabel c ){
+		if (a.getSource() == b) {
+			if (b.getText().isEmpty()) {
+				c.setText("*No puede estar vacio");
+				
+			} else if (!StringUtils.isAlpha(b.getText())){
+				c.setText("*No puede contener numeros");
+				
+			} else {c.setText(null);}}		
+	}
+	
+	public void checkIngresoNulo(FocusEvent a,JTextField b, JLabel c){
+		if (a.getSource() == b) {
+			if (b.getText().isEmpty()) {
+				c.setText("*No puede estar vacio");
+			
+			} else {c.setText(null);}}		
+	}
+	
+	public void checkIngresoAño (FocusEvent a,JTextField b, JLabel c){
+		if (a.getSource() == b) {
+			if (StringUtils.isNumeric(b.getText()) && b.getText().length()==4) {
+				c.setText(null);
+				}
+				else if(StringUtils.isAlpha(b.getText())){c.setText("*Solo numeros");
+				}
+				else {c.setText("*Debe tener 4 digitos");
+				
+				}}		
+	}
+	
+
+	
+	
+	
+	@Override
+	public void focusLost(FocusEvent arg0) {
+	
+	//REGISTRO DE USUARIO 
+		checkIngresoDni(arg0, vn.getDni_Regi(), vn.getLetras_check());
+		checkIngresoNumerico(arg0, vn.getTelefono_Regi(), vn.getLetras_check_1());
+		checkIngresoLetras(arg0, vn.getNombre_Regi(), vn.getNumeros_check());
+		checkIngresoLetras(arg0, vn.getApellido_Regi(), vn.getNumeros_check_1());
+	
+    // BUSQUEDA DE USUARIO    
+		checkIngresoNulo(arg0,vn.getUsuario_TextField_UsuarioBusqueda(),vn.getUsuario_check_Usuario());
+	    checkIngresoLetras(arg0,vn.getNombre_TextField_UsuarioBusqueda(),vn.getNombre_check_Usuario());
+	    checkIngresoLetras(arg0,vn.getApellido_TextField_UsuarioBusqueda(),vn.getApellido_check_Usuario());
+	    checkIngresoDni(arg0,vn.getDni_TextField_UsuarioBusqueda(),vn.getDni_check_Usuario());;
+	   
+	//BUSQUEDA DE PELICULAS	
+		checkIngresoAño(arg0,vn.getAño_pelicula(),vn.getAño_pelicula_check());
+	
+	}
+
+	}
+
+
+
+
+
 
 
 
 
 
 	
-}
